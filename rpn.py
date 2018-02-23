4c870f1a14e29a76dd750090a3aed56c8f95a5ff
Sam Hall <samjhall@umich.edu>
2018-02-23 10:18:43 -0500

    finished add

--- rpn.py
+++ rpn.py
@@ -9,7 +9,11 @@ def calculate(arg):
 		except ValueError:
 			arg1 = stack.pop()
 			arg2 = stack.pop()
-			return arg1 + arg2
+			result = arg1 + arg2
+			stack.append(result)
+		print(stack)
+
+	return stack.pop()
 	
 def main():
 	while True:
